210980d620195c4c98b8b15281cd76b95d951cc8
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-09-02 16:10:10 +0530

    feat(router): Added route for / and /new or /news and also added a middleware function to catch any invalid url.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
-
+import { messages } from "../message.js";
 const indexRouter = Router();
 
-
 indexRouter.get("/",(req,res) => {
-    res.status(200).send("The request was received");
+    res.status(200).send("This will be homepage");
+})
+
+indexRouter.get("/new{s}",(req,res) => {
+    res.status(200).send("This is from where you will be sending message")
+})
+
+indexRouter.get("/{*splat}",(req,res,next) => {
+    throw new Error("Wront url");
 })
 
 export default indexRouter;
