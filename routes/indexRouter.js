1b5259bcf6a19f4cd1b4e5bdb7dff3518eebdedb
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-09-02 16:33:33 +0530

    feat(router): added ability to render view template and pass messages as arguments

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,16 +2,17 @@ import { Router } from "express";
 import { messages } from "../message.js";
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => {
-  res.status(200).send("This will be homepage");
-});
 
-indexRouter.get("/new{s}", (req, res) => {
-  res.status(200).send("This is from where you will be sending message");
-});
+indexRouter.get("/",(req,res) => {
+    res.render("index",{messages:messages});
+})
 
-indexRouter.get("/{*splat}", (req, res, next) => {
-  throw new Error("Wront url");
-});
+indexRouter.get("/new{s}",(req,res) => {
+    res.status(200).send("This is from where you will be sending message")
+})
+
+indexRouter.get("/{*splat}",(req,res,next) => {
+    throw new Error("Wront url");
+})
 
 export default indexRouter;
