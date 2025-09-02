c5eda555497ef968821f9eef1144dc99e9267c2a
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-09-02 16:09:08 +0530

    feat(server): view engine, static assets and an error middleware function added

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import indexRouter from "./routes/indexRouter.js";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 dotenv.config();
-
 const app = express();
+const _fileName = fileURLToPath(import.meta.url);
+const _dirName = path.dirname(_fileName);
+const assetsPath = path.join(_dirName,"public");
 
+app.use(express.static(assetsPath));
+app.set("views",path.join(_dirName,"views"));
+app.set("view engine","ejs");
 app.use("/",indexRouter);
+
+app.use((err,req,res,next) => {
+    console.log(err);
+
+    res.status(err.status || 400).send("Bad Request");
+})
 
 const PORT = process.env.PORT;
 
